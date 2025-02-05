9a3407053d9ba5b332984c9404b3f8731a5aec67
Fuad Sano <164843890+mroxygen2024@users.noreply.github.com>
2025-02-06 02:08:21 +0300

    Magnets.py

--- Magnets.py
+++ Magnets.py
@@ -1,9 +1,9 @@
-n = int(input())  # Number of magnets
-magnet_orientation = [input().strip() for _ in range(n)]  # List to store the magnet orientations
+n = int(input())  
+magnet_orientation = [input().strip() for _ in range(n)] 
 
-groups = 1  # At least one group exists because there is at least one magnet
+groups = 1  
 
-# Traverse through the magnet orientations and count the groups
+
 for i in range(1, n):
     if magnet_orientation[i] != magnet_orientation[i - 1]:
         groups += 1
